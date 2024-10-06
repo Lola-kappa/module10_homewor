21c0115d5b1a042abd7d7f72534c50659dfa8adf
Lola-kappa <lola@mail.ru>
2024-10-06 23:23:47 +0700

    add task1 and task2 and task3

---
+++ task_2.js
@@ -0,0 +1,5 @@
+const button = document.querySelector("button");
+
+button.addEventListener('click', () =>{
+    alert(`Ширина экрана: ${window.screen.width}. Высота экрана: ${window.screen.height}.`);
+})
